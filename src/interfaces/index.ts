b12fa72123b782757dc365d5af11e2ac816a8c80
Lex <unicornware@flexdevelopment.llc>
2022-09-01 02:33:19 -0400

    feat(options): options (#3)

---
+++ src/interfaces/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @file Entry Point - Interfaces
+ * @module mkbuild/interfaces
+ */
+
+export type { default as BuildEntry } from './build-entry'
+export type { default as BuildOptions } from './build-options'
