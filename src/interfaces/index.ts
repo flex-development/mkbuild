5788971b113ebf583b4e482a89ef6cb3f698902a
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-09-01 04:56:13 -0400

    feat(interfaces): `OutputFile`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -5,4 +5,5 @@
 
 export type { default as BuildEntry } from './build-entry'
 export type { default as BuildOptions } from './build-options'
+export type { default as OutputFile } from './output-file'
 export type { default as SourceFile } from './source-file'
