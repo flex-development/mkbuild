6ed4684e621f403198990ae2f93fe4729d5a0a0f
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-09-05 17:02:18 -0400

    build(patches): replace `patch-package` with `git apply`
    - yarn patch-commit is used to generate patches. patch-package cannot be used to apply those patches on install due to directory mismatches (e.g. a/index.js vs a/node_modules/globby/index.js)
    - ds300/patch-package#272
    - https://git-scm.com/docs/git-apply
    - https://yarnpkg.com/cli/patch-commit
    
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,11 +4,12 @@
 #
 # References:
 #
-# - https://github.com/ds300/patch-package
+# - https://git-scm.com/docs/git-apply
 # - https://typicode.github.io/husky
 
 if [ -f node_modules/.bin/husky ]; then
   chmod +x .husky/* && husky install
 else
-  patch-package
+  git apply --directory=node_modules/globby --reject patches/globby+13.1.2.patch
+  git apply --directory=node_modules/mlly --reject patches/mlly+0.5.14.patch
 fi
