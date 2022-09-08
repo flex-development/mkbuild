352755eacf4ab3fb39c0c7b8a3dd9f2044b0d905
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-09-08 01:36:15 -0400

    refactor(utils): `resolveAlias` signature
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/utils/__tests__/resolve-alias.functional.spec.ts
+++ src/utils/__tests__/resolve-alias.functional.spec.ts
@@ -6,47 +6,54 @@
 import path from 'node:path'
 import * as pathe from 'pathe'
 import type { Statement } from 'src/interfaces'
-import type { MatchPath } from 'tsconfig-paths'
-import {
-  createMatchPath,
-  loadConfig,
-  type ConfigLoaderSuccessResult
-} from 'tsconfig-paths'
 import testSubject from '../resolve-alias'
 
 vi.mock('pathe')
 
 describe('functional:utils/resolveAlias', () => {
-  const src: string = path.resolve('src/utils/resolve-alias.ts')
-  const specifier: string = 'src/interfaces'
-  const code: string = `import type { OutputFile } from '${specifier}'`
-
   describe('noop', () => {
-    const matcher: MatchPath = vi.fn().mockName('matcher')
+    const match: string = faker.system.filePath()
+    const source: string = faker.system.filePath()
 
     it('should do nothing if statement.specifier is empty string', () => {
       // Act
-      testSubject({ code, specifier: '' }, src, matcher)
+      testSubject({ specifier: '' } as Omit<Statement, 'type'>, match, source)
 
       // Expect
-      expect(matcher).toHaveBeenCalledTimes(0)
       expect(pathe.dirname).toHaveBeenCalledTimes(0)
       expect(pathe.relative).toHaveBeenCalledTimes(0)
     })
 
     it('should do nothing if statement.specifier is undefined', () => {
+      // Arrange
+      const statement: Statement = {
+        code: 'export declare const foo',
+        end: 24,
+        specifier: undefined,
+        start: 0,
+        type: 'declaration'
+      }
+
       // Act
-      testSubject({ code, specifier: undefined }, src, matcher)
+      testSubject(statement, match, source)
 
       // Expect
-      expect(matcher).toHaveBeenCalledTimes(0)
       expect(pathe.dirname).toHaveBeenCalledTimes(0)
       expect(pathe.relative).toHaveBeenCalledTimes(0)
     })
 
-    it('should do nothing if matcher returns undefined', () => {
+    it('should do nothing if match is empty string', () => {
+      // Arrange
+      const statement: Statement = {
+        code: "import('bar')",
+        end: 31,
+        specifier: "'bar'",
+        start: 18,
+        type: 'dynamic'
+      }
+
       // Act
-      testSubject({ code, specifier: './interfaces' }, src, matcher)
+      testSubject(statement, '', source)
 
       // Expect
       expect(pathe.dirname).toHaveBeenCalledTimes(0)
@@ -55,34 +62,30 @@ describe('functional:utils/resolveAlias', () => {
   })
 
   describe('resolve', () => {
-    const tsconfig = loadConfig(process.cwd()) as ConfigLoaderSuccessResult
-
-    const matcher: MatchPath = createMatchPath(
-      tsconfig.absoluteBaseUrl,
-      tsconfig.paths,
-      tsconfig.mainFields,
-      tsconfig.addMatchAll
-    )
+    const source: string = path.resolve('src/utils/resolve-alias.ts')
 
     describe('node_modules', () => {
       it('should resolve alias to module in node_modules directory', () => {
         // Arrange
-        const expected: string = '@flex-development/tutils/dist/enums/node-env'
-        const specifier: string = '@flex-development/tutils/enums/node-env'
-        const statement: Pick<Statement, 'code' | 'specifier'> = {
-          code: `import NodeEnv from '${specifier}'`,
-          specifier
+        const specifier: string = 'foo-package/baz'
+        const code: string = `import baz from '${specifier}'`
+        const expected: string = 'foo-package/lib/baz'
+        const statement: Omit<Statement, 'type'> = {
+          code,
+          end: code.length,
+          specifier,
+          start: 0
         }
 
         // Act
-        testSubject(statement, src, matcher)
+        testSubject(statement, path.resolve('node_modules', expected), source)
 
         // Expect
         expect(pathe.dirname).toHaveBeenCalledTimes(0)
         expect(pathe.relative).toHaveBeenCalledTimes(0)
         expect(statement.specifier).not.to.equal(specifier)
         expect(statement.specifier).to.equal(expected)
-        expect(statement.code).not.to.contain(specifier)
+        expect(statement.code).not.to.equal(code)
         expect(statement.code).to.contain(expected)
       })
     })
@@ -90,41 +93,52 @@ describe('functional:utils/resolveAlias', () => {
     describe('relative', () => {
       it('should resolve alias to module in different directory', () => {
         // Arrange
-        const statement: Pick<Statement, 'code' | 'specifier'> = {
+        const specifier: string = '#mixins/baz'
+        const code: string = `import baz from '${specifier}'`
+        const expected: string = '../mixins/baz'
+        const statement: Omit<Statement, 'type'> = {
           code,
-          specifier
+          end: code.length,
+          specifier,
+          start: 0
         }
 
         // Act
-        testSubject(statement, src, matcher)
+        testSubject(statement, path.resolve('src/mixins/baz'), source)
 
         // Expect
         expect(pathe.dirname).toHaveBeenCalledOnce()
         expect(pathe.relative).toHaveBeenCalledOnce()
         expect(statement.specifier).not.to.equal(specifier)
-        expect(statement.specifier).to.equal('../interfaces')
-        expect(statement.code).not.to.contain(specifier)
-        expect(statement.code).to.contain(statement.specifier)
+        expect(statement.specifier).to.equal(expected)
+        expect(statement.code).not.to.equal(code)
+        expect(statement.code).to.contain(expected)
       })
 
       it('should resolve alias to module in same directory', () => {
         // Arrange
-        const specifier: string = 'src/config/constants'
-        const statement: Pick<Statement, 'code' | 'specifier'> = {
-          code: `export * from '${specifier}'`,
-          specifier
+        const match: string = path.resolve('src/foo')
+        const source: string = path.resolve('src/index.ts')
+        const specifier: string = '#foo'
+        const code: string = `export * from '${specifier}'`
+        const expected: string = './foo'
+        const statement: Omit<Statement, 'type'> = {
+          code,
+          end: code.length,
+          specifier,
+          start: 0
         }
 
         // Act
-        testSubject(statement, path.resolve('src/index.ts'), matcher)
+        testSubject(statement, match, source)
 
         // Expect
         expect(pathe.dirname).toHaveBeenCalledOnce()
         expect(pathe.relative).toHaveBeenCalledOnce()
         expect(statement.specifier).not.to.equal(specifier)
-        expect(statement.specifier).to.equal('./config/constants')
-        expect(statement.code).not.to.contain(specifier)
-        expect(statement.code).to.contain(statement.specifier)
+        expect(statement.specifier).to.equal(expected)
+        expect(statement.code).not.to.equal(code)
+        expect(statement.code).to.contain(expected)
       })
     })
   })
--- src/utils/resolve-alias.ts
+++ src/utils/resolve-alias.ts
@@ -4,49 +4,33 @@
  */
 
 import * as pathe from 'pathe'
-import { MODULE_EXTENSIONS } from 'src/config/constants'
-import type { OutputFile, Statement } from 'src/interfaces'
-import type { MatchPath } from 'tsconfig-paths'
+import type { Statement } from 'src/interfaces'
 
 /**
  * Resolves a path alias in `statement.code` and `statement.specifier`.
  *
- * @param {Pick<Statement, 'code' | 'specifier'>} statement - Statement object
- * @param {OutputFile['src']} src - Full path to output source file
- * @param {MatchPath} matcher - Path matching function
+ * @param {Omit<Statement, 'type'>} statement - Statement object
+ * @param {string} match - Possible path match
+ * @param {string} source - Full path to source file
  * @return {void} Nothing when complete
  */
 const resolveAlias = (
-  statement: Pick<Statement, 'code' | 'specifier'>,
-  src: OutputFile['src'],
-  matcher: MatchPath
+  statement: Omit<Statement, 'type'>,
+  match: string,
+  source: string
 ): void => {
-  // do nothing if missing module specifier
-  if (!statement.specifier) return
+  // do nothing if missing module specifier or path match
+  if (!statement.specifier || !match) return
 
-  /**
-   * Possible path alias match for {@link specifier}.
-   *
-   * @const {string | undefined} match
-   */
-  let match: string | undefined = matcher(
-    statement.specifier,
-    undefined,
-    undefined,
-    MODULE_EXTENSIONS
-  )
+  // remove node_modules reference or set match to relative path from src
+  match = /\/node_modules\//.test(match)
+    ? match.replace(/.+\/node_modules\//, '')
+    : pathe.relative(pathe.dirname(source), match).replace(/^(\w)/, './$1')
 
-  // resolve path match
-  if (match) {
-    // remove node_modules reference or set match to relative path from src
-    match = /\/node_modules\//.test(match)
-      ? match.replace(/.+\/node_modules\//, '')
-      : pathe.relative(pathe.dirname(src), match).replace(/^(\w)/, './$1')
-
-    // reset statement code and specifier
-    statement.code = statement.code.replace(statement.specifier, match)
-    statement.specifier = match
-  }
+  // reset statement code, end index of code, and specifier
+  statement.code = statement.code.replace(statement.specifier, match)
+  statement.end = statement.start + statement.code.length
+  statement.specifier = match
 }
 
 export default resolveAlias
