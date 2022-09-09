f77763f6812bf046833a86081e413f0f4819c682
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-09-08 20:37:14 -0400

    test(utils): use snapshots in `resolveAlias` tests
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/utils/__tests__/resolve-alias.functional.spec.ts
+++ src/utils/__tests__/resolve-alias.functional.spec.ts
@@ -67,9 +67,9 @@ describe('functional:utils/resolveAlias', () => {
     describe('node_modules', () => {
       it('should resolve alias to module in node_modules directory', () => {
         // Arrange
-        const specifier: string = 'foo-package/baz'
-        const code: string = `import baz from '${specifier}'`
-        const expected: string = 'foo-package/lib/baz'
+        const specifier: string = '@flex-development/tutils/enums/node-env'
+        const code: string = `import NodeEnv from '${specifier}'`
+        const expected: string = '@flex-development/tutils/dist/enums/node-env'
         const statement: Omit<Statement, 'type'> = {
           code,
           end: code.length,
@@ -83,19 +83,15 @@ describe('functional:utils/resolveAlias', () => {
         // Expect
         expect(pathe.dirname).toHaveBeenCalledTimes(0)
         expect(pathe.relative).toHaveBeenCalledTimes(0)
-        expect(statement.specifier).not.to.equal(specifier)
-        expect(statement.specifier).to.equal(expected)
-        expect(statement.code).not.to.equal(code)
-        expect(statement.code).to.contain(expected)
+        expect(statement).toMatchSnapshot()
       })
     })
 
     describe('relative', () => {
-      it('should resolve alias to module in different directory', () => {
+      it('should resolve alias to module in current directory', () => {
         // Arrange
-        const specifier: string = '#mixins/baz'
-        const code: string = `import baz from '${specifier}'`
-        const expected: string = '../mixins/baz'
+        const specifier: string = '#src/utils/foobar'
+        const code: string = `import { foo } from '${specifier}'`
         const statement: Omit<Statement, 'type'> = {
           code,
           end: code.length,
@@ -104,24 +100,18 @@ describe('functional:utils/resolveAlias', () => {
         }
 
         // Act
-        testSubject(statement, path.resolve('src/mixins/baz'), source)
+        testSubject(statement, path.resolve('src/utils/foobar'), source)
 
         // Expect
         expect(pathe.dirname).toHaveBeenCalledOnce()
         expect(pathe.relative).toHaveBeenCalledOnce()
-        expect(statement.specifier).not.to.equal(specifier)
-        expect(statement.specifier).to.equal(expected)
-        expect(statement.code).not.to.equal(code)
-        expect(statement.code).to.contain(expected)
+        expect(statement).toMatchSnapshot()
       })
 
-      it('should resolve alias to module in same directory', () => {
+      it('should resolve alias to module in different directory', () => {
         // Arrange
-        const match: string = path.resolve('src/foo')
-        const source: string = path.resolve('src/index.ts')
-        const specifier: string = '#foo'
-        const code: string = `export * from '${specifier}'`
-        const expected: string = './foo'
+        const specifier: string = '#src/interfaces'
+        const code: string = `import type { Statement } from '${specifier}'`
         const statement: Omit<Statement, 'type'> = {
           code,
           end: code.length,
@@ -130,15 +120,12 @@ describe('functional:utils/resolveAlias', () => {
         }
 
         // Act
-        testSubject(statement, match, source)
+        testSubject(statement, path.resolve('src/interfaces'), source)
 
         // Expect
         expect(pathe.dirname).toHaveBeenCalledOnce()
         expect(pathe.relative).toHaveBeenCalledOnce()
-        expect(statement.specifier).not.to.equal(specifier)
-        expect(statement.specifier).to.equal(expected)
-        expect(statement.code).not.to.equal(code)
-        expect(statement.code).to.contain(expected)
+        expect(statement).toMatchSnapshot()
       })
     })
   })
