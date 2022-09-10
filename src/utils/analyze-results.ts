3c136c6f07e59c5b08e323b74f86d0988b5e41c4
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-09-10 19:47:12 -0400

    feat(utils): `analyzeResults`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/utils/analyze-results.ts
@@ -0,0 +1,48 @@
+/**
+ * @file Utilities - analyzeResults
+ * @module mkbuild/utils/analyzeResults
+ */
+
+import type { Result } from '#src/interfaces'
+import * as color from 'colorette'
+import pb from 'pretty-bytes'
+
+/**
+ * Pretty prints `results`.
+ *
+ * @todo support chunks (code splitting)
+ * @todo support sourcemaps
+ *
+ * @param {string} outdir - Output directory name
+ * @param {Pick<Result, 'bytes' | 'outfile'>[]} [results=[]] - Build results
+ * @param {number} [pad=2] - Number of spaces before new lines
+ * @return {string} Log entry for `results`
+ */
+const analyzeResults = (
+  outdir: string,
+  results: Pick<Result, 'bytes' | 'outfile'>[] = [],
+  pad: number = 2
+): string => {
+  /**
+   * Total number of bytes in {@link results}.
+   *
+   * @const {number} bytes
+   */
+  const bytes: number = results.reduce((acc, result) => acc + result.bytes, 0)
+
+  /**
+   * Space before new lines.
+   *
+   * @const {string} padding
+   */
+  const padding: string = ' '.repeat(pad)
+
+  return [
+    `${padding}${color.bold(outdir)} (total size: ${color.cyan(pb(bytes))})`,
+    ...results.map(({ bytes, outfile }) => {
+      return color.gray(`${padding}└─ ${outfile} (${pb(bytes)})`)
+    })
+  ].join('\n')
+}
+
+export default analyzeResults
