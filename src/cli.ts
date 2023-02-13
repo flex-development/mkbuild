c405798a6f0a001459fede43f7898fd55a9392d0
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-02-13 14:52:01 -0500

    refactor(cli): improve error logs
    - using `esbuild.formatMessages` to format `BuildFailure` messages
    - using `consola.error` and `consola.warn` for logging
    
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/cli.ts
+++ src/cli.ts
@@ -5,6 +5,10 @@
  * @module mkbuild/cli
  */
 
+import { isNIL } from '@flex-development/tutils'
+import consola from 'consola'
+import * as esbuild from 'esbuild'
+import { get } from 'radash'
 import sade from 'sade'
 import pkg from '../package.json' assert { type: 'json' }
 import make from './make'
@@ -12,5 +16,58 @@ import make from './make'
 sade(pkg.name.replace(/.*\//, ''), true)
   .version(pkg.version)
   .describe(pkg.description)
-  .action(async (): Promise<void> => void (await make({ write: true })))
+  .action(async (): Promise<void> => {
+    try {
+      await make({ write: true })
+    } catch (e: unknown) {
+      // format and log esbuild build failure messages
+      if (!isNIL(get(e, 'errors')) && !isNIL(get(e, 'warnings'))) {
+        const { errors, warnings } = e as esbuild.BuildFailure
+
+        /**
+         * Formatted message type.
+         *
+         * @see {@linkcode esbuild.FormatMessagesOptions.kind}
+         *
+         * @const {'error' | 'warning'} kind
+         */
+        const kind: 'error' | 'warning' =
+          errors.length > 0 ? 'error' : 'warning'
+
+        /**
+         * Objects representing error or warning messages.
+         *
+         * @see {@linkcode esbuild.Message}
+         *
+         * @const {esbuild.Message[]} messages
+         */
+        const messages: esbuild.Message[] = kind === 'error' ? errors : warnings
+
+        /**
+         * Formatted error or warning messages.
+         *
+         * @see {@linkcode esbuild.formatMessages}
+         *
+         * @const {string[]} logs
+         */
+        const logs: string[] = await esbuild.formatMessages(messages, {
+          color:
+            process.env.NO_COLOR ?? process.env.NODE_DISABLE_COLORS
+              ? false
+              : process.env.FORCE_COLOR
+              ? +process.env.FORCE_COLOR > 0
+              : true,
+          kind
+        })
+
+        // log formatted build failure messages
+        for (let log of logs) {
+          log = log.replace(/^.+?].+? /, '').trim()
+          consola[kind === 'error' ? kind : 'warn'](log)
+        }
+      } else consola.error(e)
+    }
+
+    return void 0
+  })
   .parse(process.argv)
